9a0cef5829bddfe9db861423d9458059a0de6e83
Ayesh Biswal <biswalayesh3@gmail.com>
2022-08-29 21:00:49 +0530

    Complete the website

---
+++ models/author.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// Virtual for author's fullname
+AuthorSchema.virtual('name').get(function () {
+  let fullname = '';
+  if (this.first_name && this.family_name) {
+    fullname = `${this.family_name}, ${this.first_name}`;
+  }
+  if (!this.first_name || !this.family_name) {
+    fullname = '';
+  }
+  return fullname;
+});
+
+// Virtual for aurhor's URL
+AuthorSchema.virtual('url').get(function () {
+  return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual('lifespan').get(function () {
+  const birth = this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : '';
+
+  const death = this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : 'current';
+
+  return `${birth} - ${death}`;
+});
+
+// Export model
+module.exports = mongoose.model('Author', AuthorSchema);

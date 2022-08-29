5d0f028250f8b9e19e51015841649d9adfcd6569
Ayesh Biswal <biswalayesh3@gmail.com>
2022-08-29 21:10:27 +0530

    Add support for separate database

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ app.use(helmet());
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB =
+const dev_db_url =
   'mongodb+srv://local-library-user:local-library-user@cluster0.w08dwwa.mongodb.net/?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
